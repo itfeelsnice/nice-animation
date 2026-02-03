a99e6e6c629be1eba05b3b1d871cec0c52ae883a
itfeelsnice <berceabercea091234@gmail.com>
2026-02-03 18:50:37 +0200

    the begging of the car game website

---
+++ nice.js
@@ -0,0 +1,29 @@
+
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let div1 = document.querySelector("#div1");
+let h1 = document.querySelector("#a");
+
+btn1.addEventListener("click", () =>{
+    div1.classList.add("moving_car_js");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
