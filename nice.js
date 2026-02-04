f8491524095e30ed717d9d43bd227b5ffd174b7b
itfeelsnice <berceabercea091234@gmail.com>
2026-02-04 21:52:11 +0200

    added buttons made them nice and a little js

--- nice.js
+++ nice.js
@@ -1,14 +1,21 @@
 
 let btn1 = document.querySelector("#btn1");
 let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
 let div1 = document.querySelector("#div1");
 let h1 = document.querySelector("#a");
+let img1 = document.querySelector("#img1");
+let img2 = document.querySelector("#img2");
+let img3 = document.querySelector("#img3");
+
 
 btn1.addEventListener("click", () =>{
-    div1.classList.add("moving_car_js");
+    img1.classList.toggle("moving_car_js");
 });
 
-
+btn2.addEventListener("click", () =>{
+    img1.classList.toggle("rotate1");
+});
 
 
 
